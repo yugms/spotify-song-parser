185ebafbc28faab1c66828f70cc0b8a4253cd476
yugm shah <yugsha28@gmail.com>
2025-01-03 16:25:10 -0500

    refactor: move api handling to seperate file to increase readability

---
+++ api.py
@@ -0,0 +1,30 @@
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyOAuth
+
+def parse_user_id(raw_input: str) -> str:
+    '''parse a spotify user link or uri to get the user id'''
+    if "https://open.spotify.com/user/" in raw_input:
+        user_id = raw_input.split("/")[-1]
+        user_id = user_id.split("?")[0] if "?" in user_id else user_id
+    elif "spotify:user:" in raw_input:
+        user_id = raw_input.split(":")[-1]
+    else: user_id = raw_input
+    return user_id
+
+def create_playlist(name: str, user_id: str, spotify: Spotify) -> None:
+    '''create a playlist for a user'''
+    spotify.user_playlist_create(user_id, name, True, False, "")
+
+def add_item_to_playlist(tracks: list[str], playlist: str, user_id: str, sp: Spotify) -> None:
+    '''add track(s) to a playlist'''
+    sp.playlist_add_items(playlist_id=playlist, items=tracks)
+
+def search_uri(type: str, query: str, spotify: Spotify) -> str | None:
+    '''search spotify for whatever the user wants and returns uri'''
+    result = spotify.search(query, 1, 0, type, "ES")
+    return result[type]["items"][0]["uri"] if result != None else None
+
+def create_spotify_api_handler(user_id:str, client_id: str|None, client_secret: str|None, redirect_uri: str|None, scopes: list[str]) -> Spotify:
+    '''creates and returns a spotify object using spotify authorization code'''
+    auth_manager: SpotifyOAuth = SpotifyOAuth(client_id, client_secret, redirect_uri, None, scopes, ".cache-" + user_id)
+    return Spotify(auth_manager=auth_manager)
--- main.py
+++ main.py
@@ -1,20 +1,7 @@
 import os, spotipy, webbrowser
 from dotenv import load_dotenv
-from spotipy.oauth2 import SpotifyOAuth
-
-def search_track(track: str, sp: spotipy.Spotify) -> str:
-    '''search for a track on spotify'''
-    results: dict[str, dict] = sp.search(q=track, limit=1, type="track", market="ES") # type: ignore # i dont feel like writing out the entire type structure of the api call response theres no point
-    uri: str = results["tracks"]["items"][0]["uri"]
-    return uri
-
-def add_item_to_playlist(track_uris: list[str], playlist_uri: str, user_id:str, sp: spotipy.Spotify) -> None:
-    '''add track(s) to a playlist'''
-    sp.playlist_add_items(playlist_id=playlist_uri, items=track_uris)
-
-def create_playlist(user_id: str, playlist_name: str, sp: spotipy.Spotify) -> None:
-    '''create a playlist for the user'''
-    sp.user_playlist_create(user=user_id, name=playlist_name, public=True, collaborative=False)
+# custom files
+import api
 
 def check_file_validity(file_name: str) -> bool:
     '''check if a file exists and is a text file'''
@@ -46,6 +33,7 @@ def main() -> int:
     file = open(file_name, 'r')
     content = file.read()
     lines: list[str] = [line.strip() for line in content.split("\n") if not(line.isspace() or not line)] # remove empty lines
+    file.close()
 
     # check if the file is in the correct format
     if not check_file_format(content):
@@ -63,36 +51,16 @@ def main() -> int:
     if "SPOTIFY_REDIRECT_URI" not in os.environ:
         print("spotify redirect uri is not present. please create a .env file with the redirect uri variable named \"SPOTIFY_REDIRECT_URI\"")
         return 1
-    SPOTIFY_CLIENT_ID: str | None = os.getenv("SPOTIFY_CLIENT_ID")
-    SPOTIFY_CLIENT_SECRET: str | None = os.getenv("SPOTIFY_CLIENT_SECRET")
-    SPOTIFY_REDIRECT_URI: str | None = os.getenv("SPOTIFY_REDIRECT_URI")
 
     SCOPES: list[str] = [
         "playlist-modify-public", "playlist-modify-private", # needed to add songs to a playlist
         "playlist-read-private", "playlist-read-collaborative", # needed to read the playlists of the user
     ]
 
-    # authenticate with spotify
-
     print("enter the link or uri to your spotify account (go to your profile, right click on your name, and select copy spotify uri or copy link to profile) - this is needed for your spotify account id:")
-    user_id_unprocessed: str = input("")
-    user_id: str = ""
+    user_id: str = api.parse_user_id(input())
+    spotify: spotipy.Spotify = api.create_spotify_api_handler(user_id, os.getenv("SPOTIFY_CLIENT_ID"), os.getenv("SPOTIFY_CLIENT_SECRET"), os.getenv("SPOTIFY_REDIRECT_URI"), SCOPES)
 
-    if "https://open.spotify.com/user/" in user_id_unprocessed:
-        user_id = user_id_unprocessed.split("/")[-1]
-        user_id = user_id.split("?")[0] if "?" in user_id else user_id
-    elif "spotify:user:" in user_id_unprocessed:
-        user_id = user_id_unprocessed.split(":")[-1]
-    else: user_id = user_id_unprocessed
-
-    auth_manager: SpotifyOAuth = SpotifyOAuth(
-        client_id=SPOTIFY_CLIENT_ID,
-        client_secret=SPOTIFY_CLIENT_SECRET,
-        redirect_uri=SPOTIFY_REDIRECT_URI,
-        scope=SCOPES,
-        cache_path=".cache-" + user_id
-    )
-    sp: spotipy.Spotify = spotipy.Spotify(auth_manager=auth_manager)
     # separate the file into sections
     songs_list: list[str] = []
     artists: list[str] = []
@@ -125,7 +93,7 @@ def main() -> int:
         if "!songs" not in playlist_line:
             songs["songs"].append(song)
 
-    user_playlists_raw: list[dict] = sp.current_user_playlists(limit=50)["items"] # type: ignore # same reason as last time
+    user_playlists_raw: list[dict] = spotify.current_user_playlists(limit=50)["items"] # type: ignore # same reason as last time
     user_playlists: dict[str, str] = {}
     for item in user_playlists_raw:
         user_playlists[item["name"]] = item["uri"]
@@ -133,12 +101,12 @@ def main() -> int:
     for playlist, song in songs.items(): # type: ignore # i have no idea why its wrong or how to fix it and it works so
         if playlist not in user_playlists:
             if playlist[0] == "+":
-                create_playlist(user_id, playlist, sp)
+                api.create_playlist(playlist, user_id, spotify)
             else:
                 while playlist not in user_playlists:
                     match input(f"playlist {playlist} does not exist and is not marked to be created. do you want to create it, skip it, or change that playlist to another playlist? (1, 2, 3)"):
                         case 1:
-                            create_playlist(user_id, playlist, sp)
+                            api.create_playlist(playlist, user_id, spotify)
                             user_playlists.append(playlist)
                             break
                         case 2:
@@ -146,9 +114,15 @@ def main() -> int:
                         case 3:
                             playlist = input("enter the name of the changed playlist: ")
         elif playlist in user_playlists:
-            track_uris: list[str] = [search_track(track, sp) for track in song]
+            track_uris: list[str] = []
+            for track in song:
+                search_result: str | None = api.search_uri("track", track, spotify)
+                if search_result == None:
+                    print(f"{track} either does not exist or the inputted search was too far from the original name. skipping track.")
+                    continue
+                track_uris.append(search_result) # type: ignore # i handle this already
             playlist_uri = user_playlists[playlist]
-            add_item_to_playlist(track_uris, playlist_uri, user_id, sp)
+            api.add_item_to_playlist(track_uris, playlist_uri, user_id, spotify)
         else:
             print("unknown error")
             return 1
